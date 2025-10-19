4ada3a20ddcfd622b3118b2b9d3ede1557a87ee0
ygttncy <yigit03yigit@gmail.com>
2025-10-19 19:30:39 +0300

    update scss

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -3,7 +3,11 @@ import SecondaryHeader from "@components/SecondaryHeader/SecondaryHeader";
 function Home() {
   return (
     <SecondaryHeader
-      address="<strong>Rezilla,</strong> 18 Grattan St, Brooklyn"
+      address={
+        <>
+          <strong>Rezilla,</strong> 18 Grattan St, Brooklyn
+        </>
+      }
       phone="+1 206–214–2298"
       email="support@rezilla.com"
     />
