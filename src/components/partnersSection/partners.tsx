01fa9b55d093e92480809c90bc2f1a151526121d
nursahtuncel <nursahtuncell@gmail.com>
2025-10-22 20:11:09 +0300

    [SU-13] - move font

--- src/components/partnersSection/partners.tsx
+++ src/components/partnersSection/partners.tsx
@@ -3,26 +3,32 @@ import styles from "./partners.module.scss";
 const Partners: React.FC = () => {
   const partners = [
     {
+      id:1,
       name: "Google",
       image: "google.png",
     },
     {
+      id:2,
       name: "Amazon",
       image: "amazon.png",
     },
     {
+        id:3,
       name: "Logitech",
       image: "logitech.png",
     },
     {
+      id:4,
       name: "Spotify",
       image: "spotify.png",
     },
     {
+      id:5,
       name: "Samsung",
       image: "samsung.png",
     },
     {
+      id:6,
       name: "Netflix",
       image: "netflix.png",
     },
@@ -30,12 +36,12 @@ const Partners: React.FC = () => {
   return (
     <section className={styles.partners}>
       <div className={styles.bigBlueBlob}>
-        <img src="Ellipse.png" alt="" />
+        <img src="Ellipse.png" alt={"Ellipse"} />
       </div>
       <h6>Trusted by 100+ Companies across the globe! </h6>
       <ul className={styles.logos}>
      {partners.map((partner) => (
-  <li key={partner.image} className={styles.logo}>
+  <li key={partner.id} className={styles.logo}>
     <img src={partner.image} alt={partner.name} />
   </li>
 ))}
